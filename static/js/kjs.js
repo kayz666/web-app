bcbf2361d7b3bc302ce1a1179e147e89fdbf71ec
kay <762370895@qq.com>
2017-12-29 23:11:16 +0800

    完成注册页面的 表单验证 和 滑动验证

---
+++ static/js/kjs.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    // $.validator.setDefaults({
+    //     submitHandler: function() {
+    //
+    //     }
+    // });
+
+    $("#signinWithEmailForm").validate();
+
+    $("#registerWithEmailForm").validate({
+        rules:{
+            email:{
+                required:true,
+                email:true
+            },
+            password:{
+                required:true,
+                minlength:5
+            },
+            confirm_password:{
+                required:true,
+                minlength:5,
+                equalTo: "#password"
+            }
+        },
+        message:{
+            password:{
+                required:"please provide a password",
+                minlength:"Your password must be at least 5 characters long"
+
+            },
+            confirm_password:{
+                required:"please provide a password",
+                minlength:"Your password must be at least 5 characters long",
+                equalTo: "Please enter the same password as above"
+            },
+            email:"please enter a valid email address"
+        }
+    });
+
+});
